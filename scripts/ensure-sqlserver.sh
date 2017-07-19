e45bd96ed7c8f23e8dd4f24623397b7c4479c2fb
Joshua Lang <joshual@zillowgroup.com>
2017-07-19 11:12:09 -0700

    Try and improve the reliability of running SQL Server on Travis

--- scripts/ensure-sqlserver.sh
+++ scripts/ensure-sqlserver.sh
@@ -2,37 +2,31 @@
 
 CONTAINER=${1:-ctds-unittest-sqlserver}
 
-if [ -z `docker ps -f name=$CONTAINER -q` ]; then
-    echo "MS SQL Server docker container not running; starting ..."
-    docker build -q -f Dockerfile-sqlserver -t "$CONTAINER" .
-    docker run -d \
-           -e 'ACCEPT_EULA=Y' \
-           -e 'SA_PASSWORD=cTDS-unitest!' \
-           -e 'MSSQL_PID=Developer' \
-           -p 1433:1433 \
-           --rm \
-           --name "$CONTAINER" \
-           "$CONTAINER"
-fi
-
 HOSTNAME=localhost
-PORT=1433
 RETRIES=10
 
 USERNAME=TDSUnittest
 PASSWORD=TDSUnittest1
 
-docker ps
-
 until docker exec "$CONTAINER" \
              /bin/sh -c "/opt/mssql-tools/bin/sqlcmd -S $HOSTNAME -U $USERNAME -P $PASSWORD -Q 'SELECT @@VERSION'"
 do
+    if [ -z `docker ps -f name=$CONTAINER -q` ]; then
+        echo "MS SQL Server docker container not running; starting ..."
+        docker build -q -f Dockerfile-sqlserver -t "$CONTAINER" .
+        docker run -d \
+               -e 'ACCEPT_EULA=Y' \
+               -e 'SA_PASSWORD=cTDS-unitest!' \
+               -e 'MSSQL_PID=Developer' \
+               --rm \
+               --name "$CONTAINER" \
+               "$CONTAINER"
+    fi
     if [ "$RETRIES" -le 0 ]; then
         echo "Retry count exceeded; exiting ..."
-        docker ps
         exit 1
     fi
     RETRIES=$((RETRIES - 1))
-    echo "$(date) waiting 5s for $HOSTNAME:$PORT to start ..."
+    echo "$(date) waiting 5s for $CONTAINER to start ..."
     sleep 5s
 done
