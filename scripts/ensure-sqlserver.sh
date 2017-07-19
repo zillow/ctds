0d9f88e8fb801ed2d2cb2280e7605fbc6466fd41
Joshua Lang <joshual@zillowgroup.com>
2017-07-19 10:01:14 -0700

    Fix sqlserver container start and retry logic

--- scripts/ensure-sqlserver.sh
+++ scripts/ensure-sqlserver.sh
@@ -3,8 +3,8 @@
 CONTAINER=${1:-ctds-unittest-sqlserver}
 
 if [ -z `docker ps -f name=$CONTAINER -q` ]; then \
-    echo "MS SQL Server docker container not running; starting ..."; \
-    docker build -q -f Dockerfile-sqlserver -t "$CONTAINER" .; \
+    echo "MS SQL Server docker container not running; starting ..."
+    docker build -q -f Dockerfile-sqlserver -t "$CONTAINER" .
     docker run -d \
            -e 'ACCEPT_EULA=Y' \
            -e 'SA_PASSWORD=cTDS-unitest!' \
@@ -29,7 +29,7 @@ do
         echo "Retry count $RETRIES exceeded; exiting ..."
         exit 1
     fi
-    retries=$((retries - 1))
+    RETRIES=$((RETRIES - 1))
     echo "$(date) waiting 5s for $HOSTNAME:$PORT to start ..."
     sleep 5s
 done
