9e349c4f4a4967de9fb946afe36b0b98877856f2
Joshua Lang <joshual@zillowgroup.com>
2018-08-31 13:07:25 -0700

    Install opencppcoverage usage choco instead of downloaded installer on Appveyor

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -24,24 +24,6 @@ else
     Add-AppveyorMessage -Message "Using cached version of FreeTDS." -Category Information
 }
 
-# Download OpenCppCoverage to the cached dir.
-$OpenCppCoverageInstaller = "$env:BUILD_INSTALL_PREFIX\OpenCppCoverageSetup.exe"
-if (-not (Test-Path -Path $OpenCppCoverageInstaller))
-{
-    $Url = "https://github.com/OpenCppCoverage/OpenCppCoverage/releases/download/release-0.9.6.1/OpenCppCoverageSetup-x64-0.9.6.1.exe"
-    Add-AppveyorMessage -Message "Downloading OpenCppCodeCoverage installer ..." -Category Information
-    (New-Object System.Net.WebClient).DownloadFile($Url, $OpenCppCoverageInstaller)
-}
-else
-{
-    Add-AppveyorMessage -Message "Using cached version of OpenCppCoverage installer ..." -Category Information
-}
-
-# Run the OpenCppCodeCoverage installer.
-Add-AppveyorMessage -Message "Installing OpenCppCodeCoverage ..." -Category Information
-& "$OpenCppCoverageInstaller" /SILENT
-if ($LastExitCode -ne 0) { exit $LastExitCode }
-
 # Upgrade pip.
 & "$env:PYTHON\python.exe" -m pip install `
         --no-warn-script-location `
